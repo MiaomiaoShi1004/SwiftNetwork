69d0b161e965574db63e96e25415af62069831b0
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-19 19:34:58 +0200

    Initial Commit

---
+++ SwiftNetwork/SwiftNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkApp.swift
+//  SwiftNetwork
+//
+//  Created by Miaomiao Shi on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
