5fac453087f09139d8fce3f2d10f72794d2022ac
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-20 09:28:03 +0200

    Project Setup

--- SwiftNetwork/Core/AllCoins/View/ContentView.swift
+++ SwiftNetwork/Core/AllCoins/View/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Miao!")
+            Text("Hello, world!")
         }
         .padding()
     }
