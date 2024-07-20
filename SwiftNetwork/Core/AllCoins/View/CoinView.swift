0429b9ed5a5b74653e7d6e35a1d4b77ccb6c832f
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-20 09:32:40 +0200

    setup folder structure

---
+++ SwiftNetwork/Core/AllCoins/View/CoinView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinView.swift
+//  SwiftNetwork
+//
+//  Created by Miaomiao Shi on 20/07/2024.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinView()
+}
