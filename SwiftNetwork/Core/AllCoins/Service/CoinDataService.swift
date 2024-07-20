92df72e127fb4f2dd8af7143ba8aa52e233e5eb4
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-21 00:51:18 +0200

    passing error using completion handler using result type

--- SwiftNetwork/Core/AllCoins/Service/CoinDataService.swift
+++ SwiftNetwork/Core/AllCoins/Service/CoinDataService.swift
@@ -8,7 +8,30 @@
 import Foundation
 
 class CoinDataService {
-    private let urlString = "https://ap.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&locale=en"
+    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&locale=en"
+    // Using Result type
+    func fetchCoinsWithResult(completion: @escaping(Result<[Coin], Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else {
+                print("DEBUG: Failed to decode coins")
+                return
+            }
+
+            completion(.success(coins))
+        }.resume()
+    }
+
+    
+    
     // completion handler getting back an array of coins
     func fetchCoins(completion: @escaping([Coin]?, Error?) -> Void) {
         guard let url = URL(string: urlString) else { return }
--- SwiftNetwork/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ SwiftNetwork/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -19,13 +19,23 @@ class CoinsViewModel: ObservableObject {
     }
     
     func fetchCoins() {
-        service.fetchCoins { coins, error in
+//        service.fetchCoins { coins, error in
+//            DispatchQueue.main.async {
+//                if let error = error {
+//                    self.errorMessage = error.localizedDescription
+//                    return
+//                }
+//                self.coins = coins ?? []
+//            }
+//        }
+        service.fetchCoinsWithResult { result in
             DispatchQueue.main.async {
-                if let error = error {
+                switch result {
+                case .success(let coins):
+                    self.coins = coins
+                case .failure(let error):
                     self.errorMessage = error.localizedDescription
-                    return
                 }
-                self.coins = coins ?? []
             }
         }
     }
