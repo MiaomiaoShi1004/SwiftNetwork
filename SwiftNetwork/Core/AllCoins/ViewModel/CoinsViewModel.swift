ba21f00a7799160e45bc79f85ae71b358af71672
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-20 14:16:32 +0200

    understand thread

--- SwiftNetwork/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ SwiftNetwork/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -14,33 +14,25 @@ class CoinsViewModel: ObservableObject {
     
     init() {
         fetchPrice(coin: "litecoin")
+        fetchPrice(coin: "bitcoin")
     }
     
     func fetchPrice(coin: String) {
+        print(Thread.current)
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return } // converting a string to an actual url obj
         
         print("Fetching price...") // 1
         // Reach out to the URL and fetch data back
         URLSession.shared.dataTask(with: url) { data, response , error in
+            print(Thread.current)
+
             // completion handler - also know as call back (make a call to the api - wait some time - get back from the server
             print("Did receieve data \(data)") // 3
             
-            guard let data = data else {
-                print("failed to get data")
-                return
-            }
-            
-            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                print("failed to jsonObj")
-                return
-            }
-            
-            guard let value = jsonObject[coin] as? [String: Double] else {
-                print("failed to parse value")
-                return
-            }
-            
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            guard let value = jsonObject[coin] as? [String: Double] else { return }
             guard let price = value["usd"] else {
                 print("failed to get price")
                 return
@@ -48,6 +40,7 @@ class CoinsViewModel: ObservableObject {
 
             // updating UI has to be on main thread
             DispatchQueue.main.async {
+                print(Thread.current)
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
